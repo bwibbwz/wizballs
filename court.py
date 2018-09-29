0d2380de52acde63d602dac66c1029b0c3f84de6
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-09-29 22:36:25 +0000

    The Game area is now split into 2 areas, the court and score section. The way this is currently structured seems odd.

---
+++ court.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import pygame
+
+class CourtLine(pygame.sprite.Sprite):
+    def __init__(sub_surface)
+        pygame.sprite.Sprite.__init__(self)
+        self.sub_surface = sub_surface
+        
+
+
+
--- gameboard.py
+++ gameboard.py
@@ -5,6 +5,30 @@ import pygame
 # Import static parameters
 from conf import *
 
+from groups import CourtGroup
+
+COURT_RATIO = 0.8
+
 def draw_gameboard():
     gameSurface = pygame.display.get_surface()
-    gameSurface.fill(BG_COLOR)
+    gameSurface.fill(CL_BG)
+
+    score_board = Court(CL_SCORE, (GRID_SIZE, GRID_SIZE), (X_SIZE - 2 * GRID_SIZE, Y_SIZE - GRID_SIZE - COURT_RATIO * Y_SIZE))
+    play_court = Court(CL_COURT, (GRID_SIZE, GRID_SIZE + (1.0 - COURT_RATIO) * Y_SIZE), (X_SIZE - 2 * GRID_SIZE, Y_SIZE - 2 * GRID_SIZE - (1.0 - COURT_RATIO) * Y_SIZE))
+    courts = CourtGroup()
+    courts.add(score_board)
+    courts.add(play_court)
+    
+    courts.draw(gameSurface)
+
+class Court(pygame.sprite.Sprite):
+    def __init__(self, color, pos, size):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.Surface(size)
+        self.image.fill(color)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        
