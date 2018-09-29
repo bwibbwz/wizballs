398b6a62d7982329d85cbe09a0d7136e69fd6446
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-09-29 18:53:25 +0000

    Logger set up.

---
+++ wizballs.py
@@ -0,0 +1,7 @@
+import pygame, sys, logging
+
+# Set up logging
+logging.basicConfig(filename = 'wizballs.log', filemode = 'w', level = logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
+l = logging.getLogger()
+
+
