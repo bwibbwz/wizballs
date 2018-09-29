05c802fcbf782936a4d82fcfe532cd42c30a9e2d
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-09-29 19:08:32 +0000

    Added a configuration file for static values. Now it contains grid size values.

--- wizballs.py
+++ wizballs.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+
 import pygame, sys, logging
 
 # Set up logging
 logging.basicConfig(filename = 'wizballs.log', filemode = 'w', level = logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
 l = logging.getLogger()
 
+# Import static parameters
+from wizballs_conf import X_SIZE, Y_SIZE, GRID_SIZE
 
---
+++ wizballs_conf.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+X_SIZE = 1000
+Y_SIZE = 720
+GRID_SIZE = 20
