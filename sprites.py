8e5bfd105887ce910108f4818470e20cd7f8e1bb
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-10-02 15:29:14 +0000

    Implemented some skeleton stuff for the selectable sprites.

--- sprites.py
+++ sprites.py
@@ -7,5 +7,27 @@ from conf import *
 class SelectableSprite(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self, self.groups)
-        self.selected = False
+        self._selected = False
 
+    def select(self):
+        self._selected = True
+        self.update()
+
+    def deselect(self):
+        self._selected = False
+        self.update()
+
+    def is_selected(self):
+        return self._selected == True
+
+    def update(self):
+        if self.is_selected():
+            self.update_selected()
+        else:
+            self.update_unselected()
+
+    def update_selected(self):
+        pass
+
+    def update_unselected(self):
+        pass
