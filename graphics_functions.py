2b1d3f95ec325f589ca9d46ef3ef8f6f55deddc6
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-10-03 18:14:55 +0000

    It is now possible to calculate the average color of an image. Why? Who knows?

--- graphics_functions.py
+++ graphics_functions.py
@@ -13,3 +13,14 @@ def copy_color(color, alpha=None):
     else:
         return pygame.Color(color.r, color.g, color.b, color.a)
         
+def average_color(image):
+    pixels = [0, 0, 0]
+    for k in range(image.get_rect().w):
+        for j in range(image.get_rect().h):
+            color = image.get_at((k, j))
+            pixels[0] += color.r
+            pixels[1] += color.g
+            pixels[2] += color.b
+    avg_color = pygame.Color(round(pixels[0]/k/j), round(pixels[1]/k/j), round(pixels[2]/k/j), 255)
+    return avg_color
+        
