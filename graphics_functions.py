d5234a7e46f34a71e162751dc253b0358d38e209
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-10-03 15:52:57 +0000

    Copying colors is (for some reason) not obvious in pygame. This makes it so.

--- graphics_functions.py
+++ graphics_functions.py
@@ -6,3 +6,6 @@ from conf import *
 
 def draw_border(image, color=CL_BLACK, dimensions=(0, 0, GRID_SIZE - 1, GRID_SIZE - 1), thickness=2):
     pygame.draw.rect(image, color, dimensions, thickness)
+
+def copy_color(color):
+    return pygame.Color(color.r, color.g, color.b, color.a)
