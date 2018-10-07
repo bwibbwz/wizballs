ec39551f3cc55c5fa8cb71bd2d1268a044a4ac13
Dettifoss Bergmann <bwibbwz@gmail.com>
2018-10-07 20:17:00 +0000

    Redundant code remmoved.

--- sprites.py
+++ sprites.py
@@ -49,15 +49,3 @@ class SelectableSprite(pygame.sprite.Sprite):
 
     def is_selected(self):
         return self._selected == True
-
-    def update(self):
-        if self.is_selected():
-            self.update_selected()
-        else:
-            self.update_unselected()
-
-    def update_selected(self):
-        pass
-
-    def update_unselected(self):
-        pass
